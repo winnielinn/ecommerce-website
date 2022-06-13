5e04af82080103df9acb00ecbdbece232027b5f3
JINGWEN LIN <yaysa903@gmail.com>
2022-06-13 20:31:25 +0800

    feat: create user model

---
+++ migrations/20220613121129-create-user.js
@@ -0,0 +1,45 @@
+'use strict'
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        unique: true,
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      role: {
+        defaultValue: 'user',
+        type: Sequelize.STRING
+      },
+      order_times: {
+        defaultValue: 0,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users')
+  }
+}
