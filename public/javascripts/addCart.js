5813ff2a6ca4e00e80779a24fdeb755d2478feef
JINGWEN LIN <yaysa903@gmail.com>
2022-08-29 23:10:31 +0800

    refactor: not use IIFE in addCart.js

--- public/javascripts/addCart.js
+++ public/javascripts/addCart.js
@@ -1,16 +1,14 @@
-(function addCart () {
-  'use strict'
+const button = document.querySelector('.add-cart')
 
-  const button = document.querySelector('.add-cart')
+function addToCart (event) {
+  const list = JSON.parse(localStorage.getItem('cartItem')) || []
 
-  button.addEventListener('click', function addToCart (event) {
-    const list = JSON.parse(localStorage.getItem('cartItem')) || []
+  if (list.some(item => item === event.target.dataset.id)) {
+    return alert('該商品已在購物車中。')
+  }
 
-    if (list.some(item => item === event.target.dataset.id)) {
-      return alert('該商品已在購物車中。')
-    }
+  list.push(event.target.dataset.id)
+  localStorage.setItem('cartItem', JSON.stringify(list))
+}
 
-    list.push(event.target.dataset.id)
-    localStorage.setItem('cartItem', JSON.stringify(list))
-  })
-})()
+button.addEventListener('click', addToCart)
