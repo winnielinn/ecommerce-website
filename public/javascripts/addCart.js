5e27d827a2a7be73885b97a82b79c3feb5591221
winnielinn <yaysa903@gmail.com>
2022-11-17 22:52:12 +0800

    feat: store product quantity in localStorage

--- public/javascripts/addCart.js
+++ public/javascripts/addCart.js
@@ -3,7 +3,7 @@ const button = document.querySelector('.add-cart')
 function addToCart (event) {
   const list = JSON.parse(localStorage.getItem('cartItem')) || []
 
-  list.push(event.target.dataset.id)
+  list.push({ productId: event.target.dataset.id, productQuantity: 1 })
   localStorage.setItem('cartItem', JSON.stringify(list))
 
   const productButton = document.querySelector('.btn-group')
